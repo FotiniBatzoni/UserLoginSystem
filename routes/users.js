51090854b6ef3fb5fcc3120aff015e97ad714dcb
fotini <f.batzoni@gmail.com>
2022-10-07 12:54:25 +0300

    User Registration

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const {  check ,validationResult } = require('express-validator');
 var multer = require('multer');
-var upload = multer({ dest: './uploads'})
-
-var User = require('../Models/User')
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,95 +9,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register',{ title:'Register' });
+  res.render('register',{title:'Register'});
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login',{ title:'Login' });
+  res.render('login', {title:'Login'});
 });
 
-router.post('/register',   
-check('name')
-.notEmpty()
-.withMessage('Name is required field')
-.bail() // it's something like stop
-.isLength({ min: 4, max: 32 })
-.withMessage('Name should be at least 4 and at most 32 chars'),
-
-check('email')
-.notEmpty()
-.withMessage('Email is required field')
-.bail()
-.isEmail()
-.withMessage('Invalid Email')
-.bail(),
-
-check('username')
-.notEmpty()
-.withMessage('Username is required field')
-.bail() // it's something like stop
-.isLength({ min: 4, max: 32 })
-.withMessage('Username should be at least 4 and at most 32 chars'),
-
-check('password')
-.notEmpty()
-.withMessage('Password is a required field')
-.bail()
-.isLength({ min: 6 })
-.withMessage('Password should have at least 6 characters')
-.bail()
-.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
-.withMessage('Password should have one Capital letter, one Number and one Special Character'),
-
-check('password2')
-.equals('password')
-.withMessage('Passwords do not match')
-.bail(),
-
-upload.single('profileImage'),
- function(req, res, next) {
-
- var name = req.body.name
- var email = req.body.email
- var username = req.body.username
- var password = req.body.password
+router.post('/register', upload.single('profileimage') ,function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
 
   if(req.file){
-    console.log('Uploading File...');
-    const profileImage = req.file.filename;
-  }else{
-    console.log('No File Uploaded...');
-    const profileImage = 'noimage.jpg'
+  	console.log('Uploading File...');
+  	var profileimage = req.file.filename;
+  } else {
+  	console.log('No File Uploaded...');
+  	var profileimage = 'noimage.jpg';
   }
 
+  // Form Validator
+  req.checkBody('name','Name field is required').notEmpty();
+  req.checkBody('email','Email field is required').notEmpty();
+  req.checkBody('email','Email is not valid').isEmail();
+  req.checkBody('username','Username field is required').notEmpty();
+  req.checkBody('password','Password field is required').notEmpty();
+  req.checkBody('password2','Passwords do not match').equals(req.body.password);
 
-  //Check Errors
-  const errors =  validationResult(req);
+  // Check Errors
+  var errors = req.validationErrors();
+
   console.log(errors)
 
   if(errors){
-    res.render('register',{
-      errors: errors
-    })
-  }else{
-    var newUser = new User({
-      name:name,
-      email:email,
-      username:username, 
-      password:password,
-      profileImage:profileImage
-
-    });
-
-    User.createUser(newUser,function(err, user){
-      if(err){
-        throw err
-      }
-      console.log(user)
-    })
-
-    res.location('/');
-    res.redirect('/')
+  	res.render('register', {
+  		errors: errors
+  	});
+  } else{
+  	console.log('No Errors');
   }
 });
 
